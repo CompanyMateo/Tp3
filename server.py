6c6af7dbfc38f2227f43a9d19e0e4a8a5df796b6
CompanyMateo <mcompany@lasalleflorida.edu.ar>
2024-06-13 13:25:50 -0300

    j,hfhmgd

---
+++ server.py
@@ -0,0 +1,14 @@
+
+import socket
+# s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# s.connect (('localhost', 12345))
+# s.send("Hola, servidor".encode())
+# response = s.recv(1024).decode()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('localhost',12345))
+s.listen()
+print("Esperando conexion")
+conn, addr = s.accept()
+data = conn.recv(1024)
+conn.sendall("Message for client".encode())
