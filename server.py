9181a502d225ed43abf133d0de1f904b0033af9f
CompanyMateo <mcompany@lasalleflorida.edu.ar>
2024-06-27 12:33:38 -0300

    2.1

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect (('127.0.0.1', 12345))
 s.send("Hola, servidor".encode())
 response = s.recv(1024).decode()
-
+#comentaripoo crusiosion
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('localhost',12345))
 s.listen()
