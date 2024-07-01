817dda821aaa26dda6dd25038d8bcf9deb72ab00
CompanyMateo <mcompany@lasalleflorida.edu.ar>
2024-07-01 13:15:04 -0300

    punto5

--- client.py
+++ client.py
@@ -1,6 +1,14 @@
 import socket
+#TCP
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-s.connect(('localhost', 12346))
+s.connect(('localhost', 12346877676775))
 s.send("Hola, servidor".encode())
 response = s.recv(1024).decode()
 print(response)
+
+# UPD
+# s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+# s.connect (('localhost', 12345))
+# s.sendto("Hola, servidor".encode())
+# response = s.recv(1024).decode()
+# print("Respuesta del servidor:", response.decode())
--- server.py
+++ server.py
@@ -1,11 +1,19 @@
-
 import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('localhost',12345))
-print("Esperando conexion")
 
-while True:
-    data, client_address = s.recvfrom(1024)
-    print ("Mensaje recibido:", data.decode())
-    respuesta = "Hola, client"
-    s.sendto(respuesta.encode(), client_address)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('localhost',12345))
+s.listen()
+conn, addr = s.accept()
+data = conn.recv(1024)
+conn.sendall("Hola, client".encode())
+
+# UPD
+# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# s.bind(('localhost',12345))
+# print("Esperando conexion")
+
+# while True:
+#     data, client_address = s.recvfrom(1024)
+#     print ("Mensaje recibido:", data.decode())
+#     respuesta = "Hola, client"
+#     s.sendto(respuesta.encode(), client_address)
