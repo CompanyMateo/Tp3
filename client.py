5246731986feae952fc1402b190ea31a4ee666c3
CompanyMateo <mcompany@lasalleflorida.edu.ar>
2024-07-01 12:35:27 -0300

    punto 4

--- client.py
+++ client.py
@@ -1,14 +1,6 @@
-# import socket
-# s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-# s.connect (('localhost', 12345))
-# s.sendto("Hola, servidor".decode())
-# response, server = s.recvfrom(1024)
-# print("Respuesta del servidor:", response.decode())
-
 import socket
-
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-s.connect(('localhost', 12345))
+s.connect(('localhost', 12346))
 s.send("Hola, servidor".encode())
 response = s.recv(1024).decode()
 print(response)
